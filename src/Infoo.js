218e87916bff87eca52e0ff3a86d805e50b6fee0
zoebelete <hiwotbelete04@gmail.com>
2021-11-27 07:26:19 -0800

    all

--- src/Infoo.js
+++ src/Infoo.js
@@ -83,6 +83,32 @@ function Infoo() {
           </span>{" "}
         </div>
       </div>
+
+      <div class="rounded overflow-hidden shadow-lg">
+        <img
+          class="w-full h-96"
+          src="https://i.ibb.co/jDM0Hhg/chat.png"
+          alt="gmail clone"
+        />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2 text-gray-300">Group Chat </div>
+          <p class="text-gray-400 text-base">
+            This is a Group Chat website project built next.js, chatengine.
+          </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          {" "}
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            {" "}
+            <a href="https://github.com/zoebelete/Chat">
+              <GitHubIcon />{" "}
+            </a>{" "}
+          </span>{" "}
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            <a href="https://zoechat.netlify.app/">View website</a>{" "}
+          </span>{" "}
+        </div>
+      </div>
     </div>
 
     // <div className="flex justify-evenly items-center">
